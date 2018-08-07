21492648c70d4b15c87a9deb069653619b0c2609
Eloá Franca Verona <eloafran@bitwise.io>
2018-08-07 14:35:34 -0500

    Add new type of DatabaseError
    Adds new type of database error,
    which will be raised when
    Diesel returns a error.
    
    Signed-off-by: Eloá Franca Verona <eloafran@bitwise.io>

--- database/src/errors.rs
+++ database/src/errors.rs
@@ -16,17 +16,20 @@
  */
 
 use std;
+extern crate diesel;
 extern crate r2d2;
 
 #[derive(Debug)]
 pub enum DatabaseError {
     ConnError(r2d2::Error),
+    TransactionError(diesel::result::Error),
 }
 
 impl std::fmt::Display for DatabaseError {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         match *self {
             DatabaseError::ConnError(ref err) => write!(f, "Connection error: {}", err),
+            DatabaseError::TransactionError(ref err) => write!(f, "Transaction error: {}", err),
         }
     }
 }
@@ -35,12 +38,14 @@ impl std::error::Error for DatabaseError {
     fn description(&self) -> &str {
         match *self {
             DatabaseError::ConnError(ref err) => err.description(),
+            DatabaseError::TransactionError(ref err) => err.description(),
         }
     }
 
     fn cause(&self) -> Option<&std::error::Error> {
         match *self {
             DatabaseError::ConnError(ref err) => Some(err),
+            DatabaseError::TransactionError(ref err) => Some(err),
         }
     }
 }
@@ -49,6 +54,7 @@ impl From<DatabaseError> for String {
     fn from(err: DatabaseError) -> String {
         match err {
             DatabaseError::ConnError(ref err) => format!("Connection error: {}", err),
+            DatabaseError::TransactionError(ref err) => format!("Transaction error: {}", err),
         }
     }
 }
@@ -56,5 +62,11 @@ impl From<DatabaseError> for String {
 impl From<r2d2::Error> for DatabaseError {
     fn from(err: r2d2::Error) -> DatabaseError {
         DatabaseError::ConnError(err)
+    }
+}
+
+impl From<diesel::result::Error> for DatabaseError {
+    fn from(err: diesel::result::Error) -> DatabaseError {
+        DatabaseError::TransactionError(err)
     }
 }
